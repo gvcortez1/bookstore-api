4529ed50563c260f1894560b1699288c3c6f6a3b
gvcortez1 <gvcortez1@gmail.com>
2022-07-16 00:03:02 -0300

    findById para Categoria implementado

--- src/main/java/br/jus/gvcortez/bookstore/dominio/Livro.java
+++ src/main/java/br/jus/gvcortez/bookstore/dominio/Livro.java
@@ -11,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name="tb_livro")
 public class Livro  implements Serializable {
@@ -29,6 +31,7 @@ public class Livro  implements Serializable {
 	
 	private String texto;
 
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name="id_categoria")
 	private Categoria categoria;
---
+++ src/main/java/br/jus/gvcortez/bookstore/service/CategoriaService.java
@@ -0,0 +1,23 @@
+package br.jus.gvcortez.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.jus.gvcortez.bookstore.dominio.Categoria;
+import br.jus.gvcortez.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria findById(Long id) {
+	
+		Optional<Categoria> objCategoria = categoriaRepository.findById(id);
+		return objCategoria.orElse(null);
+	}
+
+}
